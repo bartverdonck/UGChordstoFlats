afaefe3ae9089c4b8d1d8b38030d0bddcbf9c004
naaraanjaa <spiderdoc@gmail.com>
2018-08-30 01:11:58 +0200

    Refactor main functions for readability

--- UGChordsToFlats.js
+++ UGChordsToFlats.js
@@ -1,9 +1,31 @@
-// Next note letter within [A-G]
-function nextLetter(letter) {
-    if (letter == "G") {
-        return "A";
+// Next note within [A-G].
+// The given note may be a chord part, only the first character is considered.
+function nextLetter(note) {
+    if (note[0] == 'G') {
+        return 'A';
     }
-    return String.fromCharCode(letter.charCodeAt(0) + 1);
+    return String.fromCharCode(note.charCodeAt(0) + 1);
+}
+
+// Return the flat enharmonic of the given chord part.
+function flatEnharmonic(part) {
+    if (part.length >= 2 && part[1] == '#') {
+        // input has no E# or B#, so no risk to make it wrong (Fb and Cb)
+        return nextLetter(part) + 'b' + part.substring(2);
+    } else {
+	return part;
+    }
+}
+
+// Chord names are of the form "[A-G][b#]?[^/]*(/[A-G][b#]?)?".
+// Examples: C, G7b9, F#m/C#, B/D#
+// We can split chords into 2 parts, on either side of the '/' if any
+// and process each part the same way to make its flat enharmonic.
+// For instance: "A#7/G#" becomes "Bb7/Ab".
+function chord2flat(chord) {
+    var parts = chord.split('/');
+    var flatParts = parts.map(flatEnharmonic);
+    return flatParts.join('/');
 }
 
 // Chord listeners for mouseover and mouseleave events show/hide
@@ -15,35 +37,26 @@ function deactivateMouseEvents(chordEl) {
     // no straight way to remove all listeners
     // but cloned elements have no listeners
     // chords are small nodes, so no perf issues
-    elClone = chordEl.cloneNode(true);
+    var elClone = chordEl.cloneNode(true);
     chordEl.parentNode.replaceChild(elClone, chordEl);
 }
 
-// Core function, processes all chord names within the current page.
-// Chord names are of the form "[A-G][b#]?[^/]*(/[A-G][b#]?)?".
+// Core function, processes all chords within the current page.
 // Sharp chords are replaced by their enharmonic flat chord,
 // considering both the fundamental and the bass note, if any.
-// For instance: "A#7/G#" is replaced by "Bb7/Ab".
 function sharp2flat() {
     const CHORD_CLASS = "_3L0Da"; // _3f5kI
+
     var chordEls = document.getElementsByClassName(CHORD_CLASS);
     for (var i = 0; i < chordEls.length; i++) {
         deactivateMouseEvents(chordEls[i]);
 
         var chord = chordEls[i].innerHTML;
-        var parts = chord.split("/");
-        for (var p = 0; p < parts.length; p++) {
-            if (parts[p].length >= 2 && parts[p][1] == "#") {
-                var current = parts[p][0];
-                // input has no E# or B#, so no risk to make it wrong (Fb and Cb)
-                var changed = nextLetter(current);
-                parts[p] = changed + "b" + parts[p].substring(2);
-            }
-        }
-        var newChord = parts.join("/");
-        if (newChord != chord) {
+        var flatChord = chord2flat(chord);
+
+        if (flatChord != chord) {
             chordEls[i].isContentEditable = true;
-            chordEls[i].innerHTML = newChord;
+            chordEls[i].innerHTML = flatChord;
             chordEls[i].isContentEditable = false;
         }
     }
