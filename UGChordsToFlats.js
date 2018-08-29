7e8d0e2b1d83d45c9c8547ea5ad8f067f51e4456
naaraanjaa <spiderdoc@gmail.com>
2018-08-30 00:12:21 +0200

    Add comments to core functions

--- UGChordsToFlats.js
+++ UGChordsToFlats.js
@@ -1,3 +1,4 @@
+// Next note letter within [A-G]
 function nextLetter(letter) {
     if (letter == "G") {
         return "A";
@@ -5,6 +6,11 @@ function nextLetter(letter) {
     return String.fromCharCode(letter.charCodeAt(0) + 1);
 }
 
+// Chord listeners for mouseover and mouseleave events show/hide
+// the chord diagram, with as side effect to reset chord names,
+// thus cancelling all changes made by this extension.
+// So we disable this feature, which anyway is presumably of little
+// use to people who care about flat vs sharp chords.
 function deactivateMouseEvents(chordEl) {
     // no straight way to remove all listeners
     // but cloned elements have no listeners
@@ -13,6 +19,11 @@ function deactivateMouseEvents(chordEl) {
     chordEl.parentNode.replaceChild(elClone, chordEl);
 }
 
+// Core function, processes all chord names within the current page.
+// Chord names are of the form "[A-G][b#]?[^/]*(/[A-G][b#]?)?".
+// Sharp chords are replaced by their enharmonic flat chord,
+// considering both the fundamental and the bass note, if any.
+// For instance: "A#7/G#" is replaced by "Bb7/Ab".
 function sharp2flat() {
     const CHORD_CLASS = "_3L0Da"; // _3f5kI
     var chordEls = document.getElementsByClassName(CHORD_CLASS);
